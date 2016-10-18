d42dec97cf378bfc14332221828b257ab7b6707f
Oskar Yildiz <yildizoskar@gmail.com>
2016-10-18 21:59:05 +0200

    Fixed a bunch of issues

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from 'react-native';
 
 import MapView from 'react-native-maps';
@@ -16,6 +17,9 @@ var foursquare = require('react-native-foursquare-api')({
 });
 
 
+let id = 0;
+
+
 
 
 class App extends React.Component {   
@@ -25,6 +29,154 @@ class App extends React.Component {
   
     this.state = {location: '', markers: [],};
   };
+
+  getToilets() {
+
+    var params = {
+      "ll": this.state.location.coords.latitude + "," + this.state.location.coords.longitude,
+      "query": "coffee"
+    };
+
+
+    foursquare.venues.getVenues(params)
+      .then((venues) => {
+            console.log(venues);
+        var response = venues.response.venues;
+        var tempMarkers = [
+        {
+          coordinate: {
+            latitude: 59.33732841,
+            longitude: 18.07367307,
+          },
+          key: id++,
+          title: 'Offentlig toalett Humlegården',
+        },
+        {
+          coordinate: {
+            latitude: 59.34001859,
+            longitude: 18.00489452,
+          },
+          key: id++,
+          title: 'Offentlig toalett Hornsbergstrand'
+        },
+        {
+          coordinate: {
+            latitude: 59.33494603,
+            longitude: 18.0554024,
+          },
+          key: id++,
+          title: 'Offentlig toalett Norra Bantorget'
+        },
+        {
+          coordinate: {
+            latitude: 59.33851777,
+            longitude: 18.0554032,
+          },
+          key: id++,
+          title: 'Offentlig toalett Tegnerlunden'
+        },
+        {
+          coordinate: {
+            latitude: 59.32797983,
+            longitude: 18.02593371,
+          },
+          key: id++,
+          title: 'Offentlig toalett Rålambshovsparken'
+        },
+        {
+          coordinate: {
+            latitude: 59.32555115,
+            longitude: 18.02174491,
+          },
+          key: id++,
+          title: 'Offentlig toalett Smedsuddsbadet'
+        },
+        {
+          coordinate: {
+            latitude: 59.31183078,
+            longitude: 18.02460641,
+          },
+          key: id++,
+          title: 'Offentlig toalett Liljeholmen - Katrinebergsbacken 4',
+        },
+        {
+          coordinate: {
+            latitude: 59.30758385,
+            longitude: 18.02847651,
+          },
+          key: id++,
+          title: 'Offentlig toalett Sjövikstorget'
+        },
+        {
+          coordinate: {
+            latitude: 59.29793596,
+            longitude: 17.99717244,
+          },
+          key: id++,
+          title: 'Offentlig toalett Telefonplan/ Mikrofonvägen'
+        },
+        {
+          coordinate: {
+            latitude: 59.28555077,
+            longitude: 17.96482994,
+          },
+          key: id++,
+          title: 'Offentlig toalett Fruängsplan'
+        },
+        {
+          coordinate: {
+            latitude: 59.29671574,
+            longitude: 17.93203232,
+          },
+          key: id++,
+          title: 'Offentlig toalett Bredäng/ Trissan parklek'
+        },
+        {
+          coordinate: {
+            latitude: 59.27656147,
+            longitude: 18.13131508,
+          },
+          key: id++,
+          title: 'Offentlig toalett Bagarmossen C t-bana'
+        },
+        {
+          coordinate: {
+            latitude: 59.31573162,
+            longitude: 18.08713373,
+          },
+          key: id++,
+          title: 'Offentlig toalett Stigbergsparken'
+        },
+        {
+          coordinate: {
+            latitude: 59.34743486,
+            longitude: 17.88401273,
+          },
+          key: id++,
+          title: 'Offentlig toalett Blackeberg Holbergsgatan'
+        },
+        ];
+        response.forEach((toilet) => {
+
+          tempMarkers.push({
+            coordinate: {
+              latitude: toilet.location.lat,
+              longitude: toilet.location.lng,
+            },
+            title: toilet.name,
+            key: id++,
+          });
+
+          this.setState({
+            markers: tempMarkers
+          });
+        });
+        })  
+      .catch(function(err){
+        console.log(err);
+      }); 
+
+  }
 
   watchID: ?number = null;
 
@@ -51,31 +203,29 @@ class App extends React.Component {
       }});
       location = lastPosition;
       this.setState({location:location});
+      this.getToilets();
       });
 
+    this.setState({
+      markers: [
+      ...this.state.markers,
+      ],
+    });
 
     console.log(this.state);
-    setTimeout(function() {
-
-
-    }, 3000);
-
-    if(!this.state.location) {
-      console.log("location unknown");
-    } else {
-      getToilets();
-    }
 
   };
 
 
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
   render() {
 
     var rightButtonConfig = {
       title: '+',
       handler: function onNext() {
-        alert('hello!');
       },
       style: {
       }
@@ -89,43 +239,28 @@ class App extends React.Component {
       title: 'FindMyToilet',
     };
 
-    var getToilets = function() {
-      if(this.state.location) {
-
-
-      }
-    }
-
     if(!this.state.location) {
       return(
         <View style={styles.loadingContainer}>
           <Text style={styles.loadingText}>FindMyToilet</Text>
-          <Spinner visible={true} size="large" overlayColor='rgba(255,255,255,0.5)' color="black" />
+          <Image
+            style={styles.image}
+            source={require('./app/img/littletoilet.png')}
+          />
+          <Spinner visible={true} size="large" overlayColor='rgba(255,255,255,0)' color="black" />
         </View>
       );
     } else {
-      var params = {
-        "ll": this.state.location.coords.latitude + "," + this.state.location.coords.longitude,
-        "query": "coffee"
-      };
 
-
-      foursquare.venues.getVenues(params)
-        .then(function(venues) {
-              console.log(venues);
-          })
-        .catch(function(err){
-          console.log(err);
-        }); 
       return (
         <View style={styles.container}>
+
           <MapView
             style={styles.map}
             showsCompass={false}
             showsUserLocation={true}
             showsMyLocationButton={true}
             loadingEnabled={true}
-            followsUserLocation={true}
            initialRegion={{
             latitude: this.state.location.coords.latitude,
             longitude: this.state.location.coords.longitude,
@@ -137,10 +272,12 @@ class App extends React.Component {
               <MapView.Marker
                 key={marker.key}
                 coordinate={marker.coordinate}
-                pinColor={marker.color}
+                title={marker.title}
+                image={require('./app/img/100toilet.png')}
               />
             ))}
           </MapView>
+          
             <NavigationBar style={styles.navBar} title={titleConfig} rightButton={rightButtonConfig} />
         </View>
       );
@@ -172,6 +309,11 @@ const styles = StyleSheet.create({
    },
    navBar: {    
     flex: 1,
+   },
+   image: {
+    width: 100,
+    height: 100,
+    marginTop: 30,
    }
 });
 
